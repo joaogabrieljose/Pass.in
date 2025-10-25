11ea62228385bf17196cccf78417674f5b5ed0e3
User <user@MacBook-Pro-de-User.local>
2025-10-25 14:19:50 +0100

    init pass.in

---
+++ src/test/java/dev_week/com/pass_in/PassInApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_week.com.pass_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
