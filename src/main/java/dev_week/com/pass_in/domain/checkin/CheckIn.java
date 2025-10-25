9700542c988f320ab080997a88cbbdc05c726824
User <user@MacBook-Pro-de-User.local>
2025-10-25 20:59:45 +0100

    create entity checkIn

---
+++ src/main/java/dev_week/com/pass_in/domain/checkin/CheckIn.java
@@ -0,0 +1,35 @@
+package dev_week.com.pass_in.domain.checkin;
+
+import java.time.LocalDateTime;
+
+import dev_week.com.pass_in.domain.attendees.Attendee;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "check_ins")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+    
+}
