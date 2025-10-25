bb761be4575e5c89cc94cc37877da9bf36e9818a
User <user@MacBook-Pro-de-User.local>
2025-10-25 20:43:28 +0100

    create entity event

---
+++ src/main/java/dev_week/com/pass_in/domain/event/Event.java
@@ -0,0 +1,36 @@
+package dev_week.com.pass_in.domain.event;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "events")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String details;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(nullable = false, name = "maximum_attendees")
+    private Integer maximumAttendees;
+    
+}
